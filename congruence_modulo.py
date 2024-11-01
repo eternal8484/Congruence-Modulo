0aa73126fa22a2bd5d18af92a3f6998d58627176
eternal8484 <akshitananya202@gmail.com>
2024-11-01 09:46:43 +0530

    Update congruence_modulo.py

--- congruence_modulo.py
+++ congruence_modulo.py
@@ -3,7 +3,7 @@ mod = int(input("What mod you want to use?\n"))
 
 def congruence_modulo():
     remainder = n%mod
-    result = f'{n} ≡ {remainder} mod({mod})'
+    result = f'{n} ≡ {remainder} mod({mod})' #WE CAN ALSO USE THIS PIECE OF CODE TO GENERATE TRIGRAM SYMBOL '\u2630'
     return result
     
 Result = congruence_modulo()
